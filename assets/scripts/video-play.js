fcc650ecacf0485f550c69c8d0d687f0f3658235
sepalaznik <se.palaznik@yandex.by>
2021-01-28 11:14:14 +0200

    add project to repositorium

---
+++ assets/scripts/video-play.js
@@ -0,0 +1,16 @@
+$(function () {
+  var $videoContainer = $('#video'),
+    $videoControls = $('.video-control'),
+    $video = $('#myVideo')[0];
+
+  $videoControls.click(function () {
+    if ($video.paused) {
+      $video.play();
+      $videoContainer.addClass('video-is-playing');
+    } else {
+      $video.pause();
+      $videoContainer.removeClass('video-is-playing');
+      $video.load();
+    }
+  });
+});
